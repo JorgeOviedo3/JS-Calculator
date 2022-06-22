d520b1675942fde2669fe976d1ba9f34feabf2cc
JorgeOviedo3 <107929074+JorgeOviedo3@users.noreply.github.com>
2022-06-22 15:09:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      display.innerText = display.innerText.substr(
+        0,
+        display.innerText.length - 1
+      );
+    } else if (item.id == "equal" && display.innerText != "") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Zero equals zero :)";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
